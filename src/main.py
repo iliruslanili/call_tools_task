b802940024feb6e4ad572caee992e71de2ddc27e
iliruslanili <73334548+iliruslanili@users.noreply.github.com>
2024-09-10 22:34:23 +0300

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -13,9 +13,9 @@ app = FastAPI()
 
 @app.post('/modify_audio_file')
 async def modify_audio_file(
-        file: UploadFile = File(media_type='audio/wav'),
-        speed: float = Form(le=2, gt=0, default=1),
-        volume: int = Form(le=200, gt=0, default=100)
+        file: UploadFile = File(media_type='audio/wav', description='Звуковая дорожка для изменения'),
+        speed: float = Form(le=2, gt=0, default=1, description='Множитель скорости: 2 - в 2 раза быстрее, 0.5 - в 2 раза медленнее'),
+        volume: int = Form(le=200, gt=0, default=100, description='Множитель громкости: 200 - в 2 раза громче, 50 - в 2 раза тише')
 ):
     x, sr = librosa.load(file.file)
 
@@ -25,16 +25,20 @@ async def modify_audio_file(
     print(sr, target_sample_rate)
 
     x = x * target_volume
-    x = librosa.resample(x, orig_sr=sr, target_sr=target_sample_rate)
+    # x_hat = librosa.resample(x, orig_sr=sr, target_sr=target_sample_rate)
 
     result = io.BytesIO()
-    soundfile.write(result, samplerate=44100, data=x, format='wav')
+    soundfile.write(result, samplerate=target_sample_rate, data=x, format='wav')
 
     headers = {'Content-Disposition': f'attachment; filename="{file.filename}"'}
     return Response(content=result.getvalue(), media_type='application/octet-stream', headers=headers)
     # return FileResponse(file_path, media_type='audio/wav')
     # return ORJSONResponse(content=data)
     # return file
+
+@app.post('/stt')
+async def stt(file : UploadFile = File(description='Звуковая дорожка для изменения')):
+    pass
 
 
 if __name__ == '__main__':
